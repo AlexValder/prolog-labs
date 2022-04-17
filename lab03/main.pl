cbcb2da16b148c9e9bff0960f1ef0d3e70c3e32a
AlexValder <anna.loban@yahoo.com>
2022-04-17 17:36:39 +0200

    DONE!

--- lab03/main.pl
+++ lab03/main.pl
@@ -18,24 +18,31 @@ sum_path([edge(_, _, T) | Tail], A) :-
        A is B + T, !.
 
 find_optimal(Begin, Path) :-
-       search_step(Begin, [], Path).
+       search_step(Begin, [], [Last|RawPath]),
+       shortest(Last, Begin, [_|WayBack], _),
+       reverse([Last|RawPath], WayThere),
+       append(WayThere, WayBack, Path).
+
+city_reverse([], []).
+city_reverse([L|Ls], [city_reverse(Ls) | L]).
 
 all_cities(Cities) :-
-       findall(A, edge(A, _, _), RawCities),
+       findall(A, connected(A, _, _), RawCities),
        list_to_set(RawCities, Cities).
 
 search_step(_, Visited, Path) :-
-       writeln("SEARCH END"),
        all_cities(Cities),
        permutation(Visited, Cities),
-       Path is Visited, writeln("done"), !.
+       Path is Visited, !.
 search_step(Current, Visited, Path) :-
-       writeln("SEARCH MAIN"),
-       closest(Current, Visited, City, _),
-       write("Closest to "), write(Current), write(" is "), writeln(City),
-       write("Visited = "), writeln(Visited),
-%       write("Path = "), writeln(Path),
-       search_step(City, [Current|Visited], Path).
+       closest(Current, Visited, CityList, _),
+       (
+              CityList = [] -> Path = [Current|Visited] ; 
+              CityList = [C] -> search_step(C, [Current|Visited], Path)
+       ), !.
+search_step(Current, Visited, Path) :-
+       closest(Current, Visited, [], _),
+       Path is [Current|Visited].
 
 cities_to_city_dist_pairs(_, [], Out) :-
        Out = [].
@@ -60,7 +67,6 @@ closest(Fst, Visited, Snd, Dist) :-
        city_dist_pairs_to_cities(Output, CityOutput),
        subtract(CityOutput, Visited, FittingCities),
        cities_to_city_dist_pairs(Fst, FittingCities, Fitting),
-%       write("Fitting = "), writeln(Fitting),
        min_dist(Fitting, Snd, Dist), !.
 
 min_dist([], City, Dist) :-
@@ -70,6 +76,28 @@ min_dist([[A, B]], City, Dist) :-
 min_dist([[A, B] | Tail], City, Dist) :-
        min_dist(Tail, [City2], [Dist2]),
        (B < Dist2 -> City = [A], Dist = [B] ; City = [City2], Dist = [Dist2]), !.
-   
-unique([]).
-unique([X|Xs]) :- \+ memberchk(X, Xs), unique(Xs).
+
+path(A,B,Path,Len) :-
+       travel(A,B,[A],Q,Len), 
+       reverse(Q,Path).
+
+travel(A,B,P,[B|P],L) :- 
+       connected(A,B,L).
+travel(A,B,Visited,Path,L) :-
+       connected(A,C,D),
+       C \== B,
+       \+member(C,Visited),
+       travel(C,B,[C|Visited],Path,L1),
+       L is D+L1.
+
+shortest(A, B, Path, Length) :-
+       setof([P,L], path(A,B,P,L), Set),
+       Set = [_|_], % fail if empty
+       minimal(Set, [Path,Length]).
+    
+minimal([F|R],M) :- min(R,F,M).
+    
+% minimal path
+min([], M, M).
+min([[P,L]|R], [_,M], Min) :- L < M, !, min(R, [P,L], Min). 
+min([_|R], M, Min) :- min(R, M, Min).
